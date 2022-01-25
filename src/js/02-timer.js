fae2b6ccc10ebca4a9e58ad7101fcb272d28a52d
Dmitriy Voskoboynikov <voskoboynikov010684@gmail.com>
2022-01-26 00:09:35 +0200

    task2/start

---
+++ src/js/02-timer.js
@@ -0,0 +1,22 @@
+// Описан в документации
+import flatpickr from 'flatpickr';
+// Дополнительный импорт стилей
+import 'flatpickr/dist/flatpickr.min.css';
+
+const inputEl = document.querySelector('#datetime-picker');
+
+const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+        console.log(selectedDates[0]);
+    },
+};
+
+inputEl.addEventListener('focus', onInputChange);
+
+function onInputChange(evt) {
+    flatpickr(inputEl, options);
+}
